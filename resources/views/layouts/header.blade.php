0dfb6c68b62d5c7da2474ee01a57dc7f5c4ebb7c
Khac Minh <minhnk@fabbi.com.vn>
2022-12-25 17:23:56 +0700

    update html css

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">Pacific<span>Travel Agency</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a href="{{ route('home.index') }}" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="{{ route('about.index') }}" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="{{ route('destination.index') }}" class="nav-link">Destination</a></li>
+                <li class="nav-item"><a href="{{ route('hotel.index') }}" class="nav-link">Hotel</a></li>
+                <li class="nav-item"><a href="{{ route('blog.index') }}" class="nav-link">Blog</a></li>
+                <li class="nav-item"><a href="{{ route('contact.index') }}" class="nav-link">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- END nav -->
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,29 @@
 */
 
 Route::get('/', function () {
-    return view('main');
-});
+    return view('home.index');
+})->name('home.index');
+
+Route::get('/destination', function () {
+    return view('destination.index');
+})->name('destination.index');
+
+Route::get('/blog', function () {
+    return view('blog.index');
+})->name('blog.index');
+
+Route::get('/blog/{id}', function () {
+    return view('blog.detail');
+})->name('blog.detail');
+
+Route::get('/contact', function () {
+    return view('contact.index');
+})->name('contact.index');
+
+Route::get('/about', function () {
+    return view('about.index');
+})->name('about.index');
+
+Route::get('/hotel', function () {
+    return view('hotel.index');
+})->name('hotel.index');
